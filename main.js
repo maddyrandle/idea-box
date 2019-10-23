fb878c112338b4a6cc15432202168fe8752f9851
Madison Randle <madisonrandle25@gmail.com>
2019-10-23 15:07:41 -0600

    Initial setup of JS

---
+++ main.js
@@ -0,0 +1,12 @@
+var formParent = document.querySelector(".idea-form");
+var cardContainerParent = document.querySelector(".empty-section-container");
+
+formParent.addEventListener("click", createCard);
+
+
+function createCard() {
+  console.log(event.target);
+  if (event.target.className === "save-btn") {
+
+  }
+}
