dd78b9ce5f8b686e3097eba2ba62d3b042d6582a
Karen Nguyen <knguyen4184@yahoo.com>
2019-10-24 18:33:37 -0600

    Work on trying to toggle star through the object class and passing throug object property in our car creation

--- main.js
+++ main.js
@@ -3,19 +3,33 @@ var cardContainerParent = document.querySelector(".empty-section-container");
 var titleInput = document.querySelector(".title-input");
 var ideaInput = document.querySelector(".body-textarea");
 var saveButton =  document.querySelector(".save-btn");
+var ideaArray = [];
 
-formParent.addEventListener("click", createCard);
+formParent.addEventListener("click", onFormParentClick);
+cardContainerParent.addEventListener("click", onCardParentClick);
 
-function createCard(event) {
+function onCardParentClick() {
+  console.log(event)
+   if (event.target.classList.contains("star-icon")) {
+   styleStar(event);
+  }
+}
+
+function onFormParentClick() {
+  instantiateIdea(titleInput.value, ideaInput.value, false);
+}
+
+
+function createCard(newIdea) {
   if (event.target.className === "save-btn" && titleInput.value && ideaInput.value) {
     var cardHtml = `
-    <div class="card-container">
+    <div id="${newIdea.id}" class="card-container">
       <header class="card-header">
-        <img src="./images/star.svg" alt="star favorite button">
+        <img class="star-icon" src="${checkStar(newIdea)}" alt="star favorite button">
         <img src="./images/delete.svg" alt="delete button">
       </header>
-      <h2 class="idea-title-heading">${titleInput.value}</h2>
-      <p class="card-idea-paragraph">${ideaInput.value}</p>
+      <h2 class="idea-title-heading">${newIdea.title}</h2>
+      <p class="card-idea-paragraph">${newIdea.body}</p>
       <footer class="card-footer">
         <img src="./images/comment.svg" alt="comment button">
         <p class="card-comment">Comment</p>
@@ -25,4 +39,37 @@ function createCard(event) {
     cardContainerParent.insertAdjacentHTML("afterbegin", cardHtml);
     formParent.reset();
   }
+}
+
+function instantiateIdea(title, body, star) {
+  var idea = new Idea(title, body, star);
+  ideaArray.push(idea);
+  createCard(idea);
+}
+
+// function findIndexOfIdea(event) {
+//   var ideaId = parseInt(event.target.classList.contains("card-container").id);
+//     for (var i = 0; i < ideaArray.length; i++) {
+//       if (ideaArray[i].id === ideaId) {
+//         return ideaArray[i];
+//       }
+//     }
+// }
+
+function styleStar(event) {
+  var hiddenObj = findIndexOfIdea(event);
+    hiddenObj.toggleStar();
+    if (hiddenObj.star) {
+      event.target.src = "./images/star-active.svg";
+    } else {
+      event.target.src = "./images/star.svg";
+    }
+}
+
+function checkStar(newIdea) {
+  if (newIdea.favorite) {
+    return "./images/star-active.svg"
+  } else {
+    return "./images/star.svg"
+    }
 }
